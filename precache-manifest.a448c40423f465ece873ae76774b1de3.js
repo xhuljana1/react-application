5816d020e0ae93317f49b64c46dc7627aed8a11e
Xhuljana Muskaj <xhuljana.muskaj@iceshpk.al>
2018-12-05 20:34:04 +0100

    Updates

---
+++ precache-manifest.a448c40423f465ece873ae76774b1de3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba8c1c99cc5049b98f2",
+    "url": "/react-application/static/js/runtime~main.2ba8c1c9.js"
+  },
+  {
+    "revision": "245e6b792e7ab9af7075",
+    "url": "/react-application/static/js/main.245e6b79.chunk.js"
+  },
+  {
+    "revision": "963675baffc169c27fe2",
+    "url": "/react-application/static/js/1.963675ba.chunk.js"
+  },
+  {
+    "revision": "245e6b792e7ab9af7075",
+    "url": "/react-application/static/css/main.915d0d73.chunk.css"
+  },
+  {
+    "revision": "bcce2ee0168b13222ee384470d859120",
+    "url": "/react-application/index.html"
+  }
+];
